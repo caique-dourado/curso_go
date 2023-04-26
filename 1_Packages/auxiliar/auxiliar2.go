d5ccd29ca18ccbd16d24f713393fbca2ff2c340e
Caique Dourado <eng.caiquedourado@gmail.com>
2023-04-26 16:39:51 -0300

    organized files

--- 1_Packages/auxiliar/auxiliar2.go
+++
@@ -1,9 +0,0 @@
-package auxiliar
-
-import "fmt"
-
-func escrever2() {
-
-	fmt.Println("escrevendo do arquivo auxilar 2 com a funçaão privada")
-
-}
--- 1_Packages/main.go
+++ 1_Packages/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"modulo/1_Packages/auxiliar"
 )
 
 func main() {
 	fmt.Println("Escrevendo do pacote main")
-	auxiliar.Escrever()
 }
