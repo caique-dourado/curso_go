51233cc80adb61ffc1ea5729efeca2eb46955019
Caique Dourado <eng.caiquedourado@gmail.com>
2023-05-08 16:36:32 -0300

    crud

---
+++ 23_CRUD/banco/banco-de-dados.go
@@ -0,0 +1,33 @@
+package banco
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// A função externa Conectar retorna uma conexao(*sql.DB) ou um erro (error)
+func Conectar() (*sql.DB, error) {
+	//string de conexão
+	uri := "user:@tcp(localhost:3306)/databasename?charset=utf8mb4&Time=True&Loc=Local"
+
+	//abrindo uma conexão
+	db, erro := sql.Open("mysql", uri)
+
+	//verificando erros externos
+	if erro != nil {
+		//retorna o valor 0 para o ponteiro *sql.DB e um erro para o error
+		return nil, erro
+	}
+
+	//validando a conexão
+	if erro = db.Ping(); erro != nil {
+		///retorna o valor 0 para o ponteiro *sql.DB e um erro de conexão para o error
+		return nil, erro
+
+	}
+
+	//SE TUDO DER CERTO ↓
+	//retorna o banco para o ponteiro *sql.DB e  o valor 0(em branco) para o error
+	return db, nil
+}
